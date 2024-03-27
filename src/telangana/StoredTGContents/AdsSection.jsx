1030689a72f8a5edaf42d4f84738d3898a331acb
Shivaramakrishna Vadugu <shivaramakrishnavadugu.ma@gmail.com>
2024-03-27 17:29:55 +0530

    update

--- src/telangana/StoredTGContents/AdsSection.jsx
+++ src/telangana/StoredTGContents/AdsSection.jsx
@@ -4,7 +4,7 @@ function AdsSection() {
     return(
         <div className="ads_section_main_tag">
             <div className="ads_section">
-                <h1>Ads Section</h1>
+                
             </div>
         </div>
     )
